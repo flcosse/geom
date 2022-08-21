7a38258e8f4a306af44b435b7b4de24c12794e45
Florian Cosse <floriancse@gmail.com>
2022-08-21 20:58:27 +0200

    leaflet app

--- leaflet.js
+++ leaflet.js
@@ -7,33 +7,6 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r
 }).addTo(map);
 
 map.on('click', function(e) {
-    var coord = e.latlng;
-    var lat = coord.lat;
-    var lng = coord.lng;
-    let request = new XMLHttpRequest();
-
-    request.open('POST', "https://api.openrouteservice.org/v2/isochrones/driving-car");
-
-    request.setRequestHeader('Accept', 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8');
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.setRequestHeader('Authorization', '5b3ce3597851110001cf6248df22650771d94c06a73d6fb78ba4706b');
-
-    request.onreadystatechange = function() {
-        if (this.readyState === 4) {
-            console.log('Status:', this.status);
-            console.log('Headers:', this.getAllResponseHeaders());
-            console.log('Body:', this.responseText);
-        }
-    };
-    
-    const body = `{"locations":[[${String(lng)},${String(lat)}]],"range":[300]}`;
-    request.send(body);
-    map.panTo(new L.LatLng(lat, lng));
-    var marker = L.marker([lat, lng]).bindTooltip(`Centre de l'isochrone<br>Latitude : ${Math.round(lat * 1000) / 1000}<br>Longitude : ${Math.round(lng * 1000) / 1000}`).addTo(map);
-});
-
-
-document.getElementById("button").onclick = function() {
 
     const velo = document.getElementById("velo");
     const voiture = document.getElementById("voiture");
@@ -46,6 +19,30 @@ document.getElementById("button").onclick = function() {
     } else if (pieds.checked == true) {
         mode = "foot-walking"
     }
-    incr = document.getElementById("test").value
 
-}
+    var coord = e.latlng;
+    var lat = coord.lat;
+    var lng = coord.lng;
+    let request = new XMLHttpRequest();
+
+    request.open('POST', `https://api.openrouteservice.org/v2/isochrones/${mode}`);
+
+    request.setRequestHeader('Accept', 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8');
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.setRequestHeader('Authorization', '5b3ce3597851110001cf6248df22650771d94c06a73d6fb78ba4706b');
+
+    request.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            console.log('Status:', this.status);
+            console.log('Headers:', this.getAllResponseHeaders());
+            console.log('Body:', this.responseText);
+        }
+    };
+
+    const body = `{"locations":[[${String(lng)},${String(lat)}]],"range":[300]}`;
+    request.send(body);
+    map.panTo(new L.LatLng(lat, lng));
+    var marker = L.marker([lat, lng]).bindTooltip(`Centre de l'isochrone<br>Latitude : ${Math.round(lat * 1000) / 1000}<br>Longitude : ${Math.round(lng * 1000) / 1000}`).addTo(map);
+
+    console.log(mode)
+});
