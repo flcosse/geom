2b465ccd598a9a9a5b5f3180a59b5823bab011b0
Florian Cosse <floriancse@gmail.com>
2022-08-24 02:09:40 +0200

    png icon

--- leaflet.js
+++ leaflet.js
@@ -136,5 +136,18 @@ legend.addTo(map);
 
 
 document.getElementById("del").onclick = function(){
-    window.location.reload();
+    map.eachLayer(function (layer) {
+        map.removeLayer(layer);
+    });
+    L.tileLayer('https://{s}.tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+	attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	minZoom: 0,
+	maxZoom: 22,
+	subdomains: 'abcd',
+	accessToken: 'cHwHz2jFd1k6blmFA6wnYur05s8mCVw6336l2GHmEEAWqvCNZ0dfQMazW83EJUHw'
+}).addTo(map);
+
+
+
+
 }
