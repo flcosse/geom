dc5ad5c759aac859ae7cb5fec838b4391bd31c86
Florian Cosse <floriancse@gmail.com>
2022-08-24 13:50:58 +0200

    leaflet geocode

--- leaflet.js
+++ leaflet.js
@@ -151,3 +151,18 @@ document.getElementById("del").onclick = function() {
     });
     tile.addTo(map);
 }
+
+var geocoder = L.Control.geocoder({
+    defaultMarkGeocode: false
+  })
+    .on('markgeocode', function(e) {
+      var bbox = e.geocode.bbox;
+      var poly = L.polygon([
+        bbox.getSouthEast(),
+        bbox.getNorthEast(),
+        bbox.getNorthWest(),
+        bbox.getSouthWest()
+      ]);
+      map.fitBounds(poly.getBounds());
+    })
+    .addTo(map);
