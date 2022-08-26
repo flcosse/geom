195ab4eb6be091f881c9c2a8e0272761d8201ef6
Florian Cosse <floriancse@gmail.com>
2022-08-26 16:17:00 +0200

    paragraphe

--- leaflet.js
+++ leaflet.js
@@ -1,12 +1,12 @@
 var map = L.map('map', {dragging: !L.Browser.mobile}).setView([48.856614, 2.3522219], 8);
 
-var tile = L.tileLayer('https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+var tile = L.tileLayer('https://{s}.tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
 	attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	minZoom: 0,
 	maxZoom: 22,
 	subdomains: 'abcd',
 	accessToken: 'cHwHz2jFd1k6blmFA6wnYur05s8mCVw6336l2GHmEEAWqvCNZ0dfQMazW83EJUHw'
-})
+});
 tile.addTo(map);
 
 var pan = document.getElementById("pan");
@@ -20,8 +20,8 @@ document.getElementById("pan").onclick = function() {
 
 var col15 = "#4d7adb"
 var col30 = "#88db4d"
-var col45 = "#dbb84d"
-var col60 = "#db4d4d"
+var col45 = "#FFD93D"
+var col60 = "#FF6B6B"
 var col10 = "#4dcfdb"
 
 document.getElementById("gen").onclick = function() {
@@ -102,7 +102,7 @@ document.getElementById("gen").onclick = function() {
 
                     layer.on('mouseover', function() {
                         this.setStyle({
-                            color: 'white',
+                            color: 'rgb(80, 80, 80)',
                             fillColor:couleur
                         });
                     });
