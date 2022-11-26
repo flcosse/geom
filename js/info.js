b05aa22bb69d22cb1d9f55b2cb7859c2f8ae0419
Florian Cosse <floriancse@gmail.com>
2022-11-26 18:54:54 +0100

    css

--- js/info.js
+++ js/info.js
@@ -1,4 +1,4 @@
-var map = L.map('map', {dragging: !L.Browser.mobile}).setView([48.856614, 2.3522219], 8);
+var map = L.map('map', {dragging: !L.Browser.mobile}).setView([46.856614, 2.3522219], 5);
 
 var tile = L.tileLayer('https://{s}.tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
 	attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
