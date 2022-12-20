c362d834e71cb7d4744a9ec3d0c0244453372725
Florian Cosse <floriancse@gmail.com>
2022-12-20 19:31:32 +0100

    leaflet

--- python/ex2.py
+++ python/ex2.py
@@ -1,24 +1,27 @@
-import os
 lieux = {
-    "Musée":[25,"Visite",25],
-    "Centre commercial":[5,"Shopping",15],
-    "Stade":[100,"Football",50],
-    "Parc":[50,"Promenade",5]
+    "Musée du Louvre":[1000,"Visite",10],
+    "Centre commercial":[500,"Shopping",75],
+    "Stade":[2000,"Football",50],
+    "Parc":[50,"Balade",5],
+    "Bibliothèque":[200,"Lecture",15],
+    "Bar":[750,"Soirée",25]
 }
 
-print("Activités proposées :\n")
-
-stype = input("Activité 1 : ").title()
+print("Activités proposées \n")
+for i in lieux:
+    print(f"{lieux[i][1]} - {lieux[i][2]}€")
+    
+stype = input("\nActivité 1 : ").title()
 stype2 = input("Activité 2 : ").title()
 budget = float(input("Budget : "))
-sorties = [] 
+sorties = []
 
 for key, values in lieux.items():
-    if budget >= values[2] and stype ==  values[1] or budget >= values[2] and stype2 ==  values[1]:
+    if (budget >= values[2] and stype ==  values[1]) or (budget >= values[2] and stype2 ==  values[1]):
         sorties.append([key, values[0], values[1], values[2]])
-    elif budget*1.1 >= values[2] and stype ==  values[1] or budget*1.1 >= values[2] and stype2 ==  values[1]:
-        choix = "oui"
-        if choix == input("C'est un peu cher bg, ça passe ? "):
+    elif (budget*1.1 >= values[2] and stype ==  values[1]) or (budget*1.1 >= values[2] and stype2 ==  values[1]):
+        choix = input("C'est un peu cher, ça passe ? ").lower()
+        if choix == "oui":
             sorties.append([key, values[0], values[1], values[2]])
         else:
             print("Compris.\n")
@@ -26,34 +29,24 @@ for key, values in lieux.items():
 for i in sorties:
     note = []
     if i[-1] <= 25:
-        note.append(15)
+        note.append(5)
     else:
-        note.append(12)
-    if i[1] <= 50:
-        note.append(12)
+        note.append(3)
+    if i[1] <= 500:
+        note.append(5)
     else:
-        note.append(10)
-    i.append((note[0]*3 + note[1]*2) / (3+2))
+        note.append(3)
+    i.insert(0,(note[0]*3 + note[1]*2) / (5))
+    if str(i[0])[-1] == "0":
+        i[0] = int(i[0])
+
+tiret = "-"*23
+sorties.sort(reverse=True)
+
+if len(sorties) == 0:
+    print("\n0 résultat")
+else:
+    print(f"\nRésultats \n{tiret}")
 
 for i in sorties:
-    if i[0] in list(lieux.keys()):
-        lieux[i[0]] += [i[4]]
-
-aucun = []
-for items, values in lieux.items():
-    try:
-        aucun.append(values[3])
-    except:
-        pass
-
-os.system("cls")
-tiret = "-"*30
-print(f"\nRésultat : \n{tiret}")
-
-if len(aucun) == 0:
-    print("0 résultat")
-    
-for i,j in zip(list(lieux.values()),list(lieux.keys())):
-    if len(i) == 4:
-        print(f"Score : {i[-1]}\nActivité : {i[1]}\nLieux : {j}\nPrix : {i[2]}€\nDistance : {i[0]}km")
-        print(tiret)
+    print(f"Score : {i[0]}/5\nActivité : {i[3]}\nLieu : {i[1]}\nPrix : {i[4]}€\nDistance : {i[2]}m\n{tiret}")
